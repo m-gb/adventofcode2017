72e9c2062854cefccdc0e8c8fcea4d8c11fa6861
m-gb <mayu666@gmail.com>
2017-12-12 20:53:06 +0100

    fix comments

--- day3/part1.rb
+++ day3/part1.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/env ruby
 
-def spiral(input) # 17
-  root = Math.sqrt(input).ceil # Returns the rounded square root of input. 5
-  upper_boundary = root ** 2 # Returns the square of root. 25
-  boundary_steps = root - 1 # Returns the Manhattan Distance (steps) of the upper boundary. 4
-  boundary_x = boundary_steps / 2 # Returns the x coordinate of the upper boundary. 2
-  boundary_y = -(boundary_steps / 2) # Returns the y coordinate of the upper boundary. -2
-  bottom_left_corner = upper_boundary - boundary_steps # 21
-  top_left_corner = bottom_left_corner - boundary_steps # 17
-  top_right_corner = top_left_corner - boundary_steps # 13
-  bottom_right_corner = top_right_corner - boundary_steps + 1 # 10
+def spiral(input)
+  root = Math.sqrt(input).ceil # Returns the rounded square root of input.
+  upper_boundary = root ** 2 # Returns the square of root.
+  boundary_steps = root - 1 # Returns the Manhattan Distance (steps) of the upper boundary.
+  boundary_x = boundary_steps / 2 # Returns the x coordinate of the upper boundary.
+  boundary_y = -(boundary_steps / 2) # Returns the y coordinate of the upper boundary.
+  bottom_left_corner = upper_boundary - boundary_steps
+  top_left_corner = bottom_left_corner - boundary_steps
+  top_right_corner = top_left_corner - boundary_steps
+  bottom_right_corner = top_right_corner - boundary_steps + 1
   
   case
   when input >= bottom_left_corner
@@ -17,9 +17,9 @@ def spiral(input) # 17
     input_x = boundary_x - diff # x can change.
     input_y = boundary_y # y is the same as boundary's.
   when input >= top_left_corner
-    diff = bottom_left_corner - input # 21 - 17 = 4
-    input_x = boundary_x - boundary_steps # 2-4=-2
-    input_y = boundary_y + diff # -2+4=2
+    diff = bottom_left_corner - input
+    input_x = boundary_x - boundary_steps
+    input_y = boundary_y + diff
   when input >= top_right_corner
     diff = top_left_corner - input
     input_x = diff - boundary_x
